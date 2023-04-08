362c2e452ac08de045505b780ae3bea67d1baa1a
Sang jin Lee <w1004123@gmail.com>
2023-04-09 01:17:47 +0900

    실시간 이름표시

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import Main from './components/MainPage';
 import data from './data';
@@ -7,11 +7,32 @@ import Cart from './components/Cart';
 import { Routes, Route, Link , useNavigate, Outlet } from 'react-router-dom';
 import Detail from './components/Detail';
 import axios from 'axios'; //라이브러리
+import { useQuery } from 'react-query';
 
 function App() {
+  let obj = {name: 'kim'}
+  localStorage.setItem('data', JSON.stringify(obj));
+  let out = localStorage.getItem('data');
+  console.log(JSON.parse(out));
+
+  useEffect(() => {
+    localStorage.setItem('data', JSON.stringify( [] ))
+  }, [])
+
+  
+
   const [counter, setCounter] = useState(1);
   const [shoes, setShoes] = useState(data);
   let navigate = useNavigate();
+
+  let result = useQuery(() => {
+    axios.get('https://codingapple1.github.io/userdata.json').then((a) => {
+      return a.data
+    })
+  })
+  console.log(result.data);
+  console.log(result.isLodaing);
+  console.log(result.error);
 
   return (
     <div className="App">
@@ -23,6 +44,7 @@ function App() {
             <Link className="links" to='/about'>회사 정보</Link>
             <Link className="links" to='/Cart'>쇼핑 카트</Link>
           </Nav>
+          <Nav className="ms-auto">{ result.isLodaing ? '로딩중' : result.data.name }</Nav>
         </Container>
       </Navbar>
 
