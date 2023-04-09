371da7de8a6aed0476ecddf1d743b0381bef1e86
Sang jin Lee <w1004123@gmail.com>
2023-04-09 11:39:34 +0900

    memo

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState, memo, useMemo } from 'react'
 import Table from 'react-bootstrap/Table';
 import { useSelector } from 'react-redux';
 
+const Child = memo (function(){
+  console.log('Child');
+  return <div>자식임</div>
+}); // 꼭 필요할때 재 렌더링 해주세요
+
+const 함수 = () => {
+  return <div>반복문 10억번</div>
+}
 function Cart() {
 
+  let result = useMemo(() => {return 함수}, []);
   let state = useSelector((state) => { return state }) //store의 모든 state가 저장됨 //state의 내용도
   console.log(state);
+  const [count, setCount] = useState(0);
 
   return (
     <Table striped>
+      <Child count={count}></Child>
+      <button onClick={() => { setCount(count + 1) }}>+</button>
       <thead>
         <tr>
           <th>#</th>
